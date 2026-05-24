baf8893a2ef0b10654d1a0844bcacaf758230e19
Kevin <ksbisht941@gmail.com>
2026-05-24 20:46:56 +0530

    Add FastAPI server for plant disease prediction
    - Created serve_api.ipynb for testing FastAPI endpoints.
    - Implemented /ping endpoint to check server status.
    - Added /predict endpoint to handle image uploads for prediction.
    - Integrated TensorFlow model loading and prediction logic.
    - Included necessary packages: fastapi, uvicorn, numpy, pillow.

---
+++ deep-learning/cnn/projects/plant_village/serve_api.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.testclient import TestClient
+import uvicorn
+
+from io import BytesIO
+from PIL import Image
+import numpy as np
+
+import tensorflow as tf
+
+
+MODEL = tf.keras.models.load_model("./models/plant_village_v1")
+CLASS_NAMES = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot', 'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy']
+
+app = FastAPI()
+client = TestClient(app)
+
+
+@app.get("/ping")
+async def ping():
+    return 'Hello, I am alive'
+
+
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+    print(predictions)
+    pass
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port="8000")
